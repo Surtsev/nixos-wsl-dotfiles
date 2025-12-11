c0ce694146d0f5b3d1e42aedd80c3a39fb0cd0f0
t0m_sUrt <surtsev2006@gmail.com>
2025-12-11 15:39:24 +0300

    Add flake.nix with home-manager configuration

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "NixOS WSL dotfiles with Home Manager and flakes";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      homeConfigurations = {
+        user = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+
+          modules = [
+            ./home.nix
+          ];
+        };
+      };
+
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          nixpkgs-fmt
+          nix-linter
+        ];
+      };
+    };
+}
