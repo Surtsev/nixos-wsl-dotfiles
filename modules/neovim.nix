5fbab9b07d3a3f90a3528decb95696dffac80546
Surtsev <surtsev2006@gmail.com>
2026-02-09 05:54:11 +0000

    add neovim transparency

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -84,6 +84,20 @@ highlight PmenuSel ctermfg=15 ctermbg=4 guifg=#ffffff guibg=#5b9bd5
 highlight PmenuThumb ctermbg=7 guibg=#5b9bd5
 highlight PmenuSbar ctermbg=8 guibg=#2e2e3e
 
+" === Transparent background ===
+highlight Normal       ctermbg=none guibg=none
+highlight NonText      ctermbg=none guibg=none
+highlight NormalFloat  ctermbg=none guibg=none
+highlight FloatBorder  ctermbg=none guibg=none
+highlight SignColumn   ctermbg=none guibg=none
+highlight LineNr       ctermbg=none guibg=none
+highlight CursorLineNr ctermbg=none guibg=none
+highlight StatusLine   ctermbg=none guibg=none
+highlight StatusLineNC ctermbg=none guibg=none
+highlight TabLine      ctermbg=none guibg=none
+highlight VertSplit    ctermbg=none guibg=none
+highlight WinSeparator ctermbg=none guibg=none
+
 " ============================================================================
 " LEADER KEY
 " ============================================================================
